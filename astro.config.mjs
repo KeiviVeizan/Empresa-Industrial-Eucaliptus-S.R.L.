aac0803307cc1a9ef5a459f4f91c7269590f5f1d
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-09-25 17:27:12 -0400

    Arreglando links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/Enpresa-Industrial-Eucaliptus-S.R.L./',
+    base: '/Empresa-Industrial-Eucaliptus-S.R.L./',
 });
