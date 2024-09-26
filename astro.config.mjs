c48077051ddff69b97f7084e7b14570af7931d05
Keivi Veizan <kveizanp@est.emi.edu.bo>
2024-09-26 17:10:16 -0400

    Arreglando componentes para el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://keiviveizan.github.io',
     base: '/Empresa-Industrial-Eucaliptus-S.R.L./',
 });
