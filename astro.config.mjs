9a34ad975a9fd7f8d0e6dd347bb15e6152b5b7fd
Keivi Veizan <kveizanp@est.emi.edu.bo>
2025-09-29 17:34:59 -0400

    arreglo de errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://keiviveizan.github.io',
-    base: '/Empresa-Industrial-Eucaliptus-S.R.L./',
+    // base: '/Empresa-Industrial-Eucaliptus-S.R.L./', // Solo para GitHub Pages
 });
